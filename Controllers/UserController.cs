d00cb8241c04bb5aead9495e2719a0007bb01ba5
HUZAIFAJUZER\Huzaifa <huzaifa.juzer@techlogix.com>
2024-06-26 21:59:39 +0500

    Updating user controller and service

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -29,5 +29,21 @@ namespace OrderManagement.Controllers
             return BadRequest(userResult.ErrorMessage);
         }
 
+        [HttpGet("GetUserById")]
+        public IActionResult GetUserById(int id)
+        {
+            var userResult = _userService.GetUserById(id);
+            if (userResult.IsSuccess)
+            {
+                return Ok(userResult.User);
+            }
+            return BadRequest(userResult.ErrorMessage);
+        }
+
+        [HttpPost("GetUserConsent")]
+        public IActionResult GetUserConsent(User user)
+        {
+            return Ok(_userService.GetUserConsent(user));
+        }
     }
 }
