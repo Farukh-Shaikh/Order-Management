3e59c0dadea440b24e85c21d9812888fee480167
HUZAIFAJUZER\Huzaifa <huzaifa.juzer@techlogix.com>
2024-06-26 15:33:57 +0500

    Testing pre-commit hook

---
+++ Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderManagement.Controllers
+{
+    public class UserController : ControllerBase
+    {
+        [HttpGet("GetNames")]
+        public IActionResult GetNames()
+        {
+            return Ok();
+        }
+
+
+    }
+}
